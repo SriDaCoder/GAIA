563a055f2fd4654c754945af9f73df27e7b5cf06
SriDaCoder <srinihasakkineni2010@gmail.com>
2025-06-27 12:36:45 +0400

    Made the GUI more unified

--- POSEIDON/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu(x) {
-  x.classList.toggle("change");
-  const panel = document.getElementById("panel");
-  if (panel.style.width === "200px") {
-    panel.style.width = "0";
-  } else {
-    panel.style.width = "200px";
-  }
-}
